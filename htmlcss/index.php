be11bc3c971af999f5716d791dc06d8a965d493e
Unknown <aaronvlak@hotmail.nl>
2019-04-16 09:33:20 +0200

    Website Prototype HTML/CSS Done
    De website is klaar qua html/css gebied.

---
+++ htmlcss/index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aaron
+ * Date: 15-4-2019
+ * Time: 11:22
+ */
+
+require 'config.php';
+require 'header.php';
+?>
+
+  <main>
+    <div class="banner">
+      <div class="container">
+
+        <img src="img/banner.jpg" alt="banner.png">
+        <div class="centered">
+          <h2>Welkom op F<span>I</span>F<span>A</span></h2>
+        </div><!-- end of centered -->
+        <div class="bottom">
+          <span>Voetbal, niet meer en niet minder.</span>
+        </div><!-- end of bottom -->
+
+      </div> <!-- end of container -->
+
+    </div> <!-- end of "banner" -->
+
+    <div class="players-and-teams">
+      <div class="container">
+
+        <div class="players">
+          <h3>Uitgelichte Spelers</h3>
+          <p>Speler Naam</p>
+          <p>Speler Naam</p>
+          <p>Speler Naam</p>
+        </div>
+
+        <div class="teams">
+          <h3>Uitgelichte Teams</h3>
+          <p>Team Naam</p>
+          <p>Team Naam</p>
+          <p>Team Naam</p>
+        </div>
+
+      </div><!-- end of container -->
+    </div><!-- end of players and teams -->
+  </main>
+
+<?php
+require 'footer.php';
+?>
