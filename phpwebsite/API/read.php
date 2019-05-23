fad69fdaf9f68d973aa80c3ef12cfba5cc54f97f
Unknown <Jaibreyon.lourens@gmail.com>
2019-05-23 08:59:40 +0200

    api

--- phpwebsite/API/read.php
+++ phpwebsite/API/read.php
@@ -3,11 +3,11 @@
 	header('Content-Type: application/json');
 	$json = json_encode($api);
 
-	echo $json;
+	
 
 	if($key == "Gr03n3Cactus")
 	{
-		//return teams
+		return $json;
 	}
 ?>
 
--- phpwebsite/API/readmatch.php
+++ phpwebsite/API/readmatch.php
@@ -4,4 +4,8 @@ header('Content-Type: application/json');
 $matchjson = json_encode($matchapi);
 echo $matchjson;
 
+if($key == "Gr03n3Cactus")
+	{
+		return $matchjson;
+	}
 ?>
